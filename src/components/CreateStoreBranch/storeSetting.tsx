1201f0f01664352784cf85971874b59c5fa4b551
Elchen Tanauan <lchentanauan@gmail.com>
2023-09-22 12:40:14 +0800

    Update Store Setting

---
+++ src/components/CreateStoreBranch/storeSetting.tsx
@@ -0,0 +1,287 @@
+import React, { useEffect, useState, useRef } from "react";
+import { TextField, Button, Grid, Paper, Typography } from "@mui/material";
+import styles from "./../Drawer/component/style/menu.module.css";
+
+interface Data {
+  _id: string;
+  storeName: string;
+  branchName: string;
+  username: string;
+  email: string;
+  address: string;
+  contactNumber: string;
+  appId: string;
+  appSecret: string;
+  photo: string;
+}
+
+const StoreInfo: React.FC = () => {
+  const [storeData, setStoreData] = useState<Data>();
+  const [photo, setPhoto] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  // const [storeName, setStoreName] = useState("");
+  // const [branchName, setBranchName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [address, setAddress] = useState("");
+  // const [contactNumber, setContactNumber] = useState("");
+  const [formData, setFormData] = useState({
+    storeName: "",
+    branchName: "",
+    email: "",
+    address: "",
+    contactNumber: "",
+    appId: "",
+    appSecret: "",
+  });
+
+  const fetchStoreData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const branch_Id = localStorage.getItem("branch_Id");
+      const store_Id = localStorage.getItem("store_Id");
+      console.log({ token, branch_Id });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/store/${store_Id}/${branch_Id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+
+      setStoreData({
+        storeName: responseData.storeName,
+        photo: responseData.photo,
+        appId: responseData.appId,
+        appSecret: responseData.appSecret,
+        branchName: responseData.branches[0].branchName,
+        email: responseData.branches[0].email,
+        contactNumber: responseData.branches[0].contactNumber,
+        address: responseData.branches[0].address,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStoreData();
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add your registration logic here
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e: any) => {
+    const selectedFile = e.target.files[0];
+    setPhoto(selectedFile);
+  };
+  const hiddenFileInput = useRef<HTMLInputElement | null>(null);
+
+  const handleUpload = () => {
+    if (hiddenFileInput.current) {
+      hiddenFileInput.current.click();
+    }
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleUpdateClick = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <Grid container justifyContent="center" alignItems="center" height="100vh">
+      <Paper
+        elevation={3}
+        style={{
+          padding: "20px",
+          width: 800,
+          paddingTop: "50px",
+          paddingBottom: "50px",
+        }}
+      >
+        {storeData ? (
+          <>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+              style={{
+                paddingLeft: 100,
+                paddingRight: 100,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  textAlign: "center",
+                }}
+              >
+                <Typography variant="h6" style={{ marginBottom: 20 }}>
+                  {isEditing ? "Update Store" : "Store Details"}
+                </Typography>
+                <div>
+                  {photo && (
+                    <div style={{ marginTop: 10 }}>
+                      <img
+                        src={URL.createObjectURL(photo)}
+                        alt="Selected"
+                        width="170"
+                        height="150"
+                        style={{ borderRadius: 10 }}
+                      />
+                    </div>
+                  )}
+
+                  <input
+                    type="file"
+                    ref={hiddenFileInput}
+                    style={{ display: "none" }}
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
+                  {isEditing && (
+                    <button
+                      className={styles.uploadImg_menu}
+                      onClick={handleUpload}
+                    >
+                      Upload Image
+                    </button>
+                  )}
+                </div>
+              </div>
+              <div>
+                <Grid container spacing={2} style={{ marginTop: 20 }}>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Store Name"
+                      variant="outlined"
+                      value={storeData.storeName}
+                      fullWidth
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      label="App ID"
+                      variant="outlined"
+                      value={storeData.appId}
+                      fullWidth
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      label="App Secret"
+                      variant="outlined"
+                      value={storeData.appSecret}
+                      fullWidth
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      label="Branch"
+                      variant="outlined"
+                      value={storeData.branchName}
+                      fullWidth
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      label="Contact No."
+                      variant="outlined"
+                      fullWidth
+                      value={storeData.contactNumber}
+                      type="email"
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Email"
+                      variant="outlined"
+                      value={storeData.email}
+                      fullWidth
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Address"
+                      variant="outlined"
+                      fullWidth
+                      multiline
+                      value={storeData.address}
+                      rows={2}
+                      required
+                      disabled={!isEditing}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} textAlign="center">
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <button
+                      onClick={isEditing ? handleUpdateClick : handleEditClick}
+                      className={
+                        isEditing ? `${styles.green}` : `${styles.save_info}`
+                      }
+                      style={{ marginTop: 10 }}
+                    >
+                      {isEditing ? "Save" : "Edit"}
+                    </button>
+                  </div>
+                </Grid>
+              </div>
+            </Grid>
+          </>
+        ) : (
+          <div>Loading</div>
+        )}
+      </Paper>
+    </Grid>
+  );
+};
+
+export default StoreInfo;
--- src/pages/admin/storeInfo/index.tsx
+++ src/pages/admin/storeInfo/index.tsx
@@ -1,5 +1,5 @@
 import TemporaryDrawer from "@/components/Drawer/Drawer";
-import StoreInfo from "@/components/Drawer/component/storeSetting";
+import StoreInfo from "@/components/CreateStoreBranch/storeSetting";
 import React from "react";
 
 const StoreSettingPage: React.FC = () => {
