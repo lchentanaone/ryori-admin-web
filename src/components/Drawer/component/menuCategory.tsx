489e193ea91b73b7a9937404dc4c781d8148a785
Elchen Tanauan <lchentanauan@gmail.com>
2023-09-22 14:19:00 +0800

    Menu Category CRUD

--- src/components/Drawer/component/menuCategory.tsx
+++ src/components/Drawer/component/menuCategory.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   TextField,
   Typography,
-  Grid,
   Paper,
   TableContainer,
   Table,
@@ -16,13 +15,114 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import styles from "../component/style/menu.module.css";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
 
+interface Categories {
+  _id: string;
+  title: string;
+  photo: string;
+}
+
 const MenuCategories = () => {
   const [title, setTitle] = useState("");
   const [photo, setPhoto] = useState(null);
-  const handleFileChange = (e: any) => {
-    const selectedFile = e.target.files[0];
-    setPhoto(selectedFile);
+  const [itemOnEdit, setItemOnEdit] = useState("");
+  const [category, setCategory] = useState<Categories[]>([]);
+
+  const fetchItems = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const store_Id = localStorage.getItem("store_Id");
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/menuCategory/?store_Id=${store_Id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const responseData = await response.json();
+      setCategory(responseData);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const handleSubmit = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const store_Id = localStorage.getItem("store_Id");
+
+      var formdata = new FormData();
+      formdata.append("title", title);
+      formdata.append("photo", photo || "");
+      formdata.append("store_Id", store_Id || "");
+      if (itemOnEdit) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/menuCategory/${itemOnEdit}`,
+          {
+            method: "PATCH",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            body: formdata,
+          }
+        );
+        if (response.ok) {
+          fetchItems();
+        } else {
+          console.error("Failed to add item.");
+        }
+      } else {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/menuCategory/`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            body: formdata,
+          }
+        );
+        if (response.ok) {
+          fetchItems();
+        } else {
+          console.error("Failed to add item.");
+        }
+      }
+      setItemOnEdit("");
+      setTitle("");
+      setPhoto(null);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const deleteItem = async (_id: string) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/menuCategory/${_id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to delete category");
+      }
+      fetchItems();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   const hiddenFileInput = useRef<HTMLInputElement | null>(null);
 
   const handleUpload = () => {
@@ -30,6 +130,22 @@ const MenuCategories = () => {
       hiddenFileInput.current.click();
     }
   };
+
+  const handleEdit = (item: any) => {
+    setItemOnEdit(item._id);
+    setTitle(item.title);
+    setPhoto(item.photo);
+  };
+
+  const handleImageChange = (e: any) => {
+    const imageFile = e.target.files[0];
+    setPhoto(imageFile);
+    if (photo) {
+      const formData = new FormData();
+      formData.append("image", photo);
+    }
+  };
+
   return (
     <Paper elevation={3} style={{ padding: "20px", height: "90vh" }}>
       <Typography variant="h6">Menu Categories</Typography>
@@ -68,9 +184,12 @@ const MenuCategories = () => {
             ref={hiddenFileInput}
             style={{ display: "none" }}
             accept="image/*"
-            onChange={handleFileChange}
+            onChange={handleImageChange}
           />
-          <button className={`${styles.save_button} ${styles.add_employee}`}>
+          <button
+            onClick={handleSubmit}
+            className={`${styles.save_button} ${styles.add_employee}`}
+          >
             Save
           </button>
         </div>
@@ -79,7 +198,11 @@ const MenuCategories = () => {
             {photo && (
               <div>
                 <img
-                  src={URL.createObjectURL(photo)}
+                  src={
+                    typeof photo === "string"
+                      ? photo
+                      : URL.createObjectURL(photo)
+                  }
                   alt="Selected"
                   width="190"
                   height="150"
@@ -100,19 +223,27 @@ const MenuCategories = () => {
                 <TableCell>Action</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Data 1</TableCell>
-                <TableCell>
-                  <IconButton aria-label="pencil">
-                    <BorderColorIcon />
-                  </IconButton>
-                  <IconButton aria-label="delete">
-                    <DeleteIcon style={{ color: "red" }} />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            </TableBody>
+            {category.map((item, index) => (
+              <TableBody key={index}>
+                <TableRow>
+                  <TableCell>{item.title}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      onClick={() => handleEdit(item)}
+                      aria-label="pencil"
+                    >
+                      <BorderColorIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => deleteItem(item._id)}
+                      aria-label="delete"
+                    >
+                      <DeleteIcon style={{ color: "red" }} />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            ))}
           </Table>
         </TableContainer>
       </div>
